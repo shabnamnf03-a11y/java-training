95820093743da77ecd8f8f929639b89be2e6000e
Shabnam <shabnamnf03@gmail.com>
2026-06-30 14:26:12 +0530

    Initial

---
+++ Day1/Test.java
@@ -0,0 +1,13 @@
+package Day1;
+
+public class Test {
+    Test() {
+        System.out.println("Constructor Executed");
+    }
+    public static void main(String[] args) {
+        new Test();
+    }
+}
+
+    
+
