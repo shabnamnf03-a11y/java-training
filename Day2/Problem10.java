63f2ba186545e7473576a6e1aa2fd59eb8089bc0
Shabnam <shabnamnf03@gmail.com>
2026-06-30 15:24:55 +0530

    Initial

---
+++ Day2/Problem10.java
@@ -0,0 +1,12 @@
+package Day2;
+
+public class Problem10 {
+   
+    public static void main(String[] args) {
+        double num = 10.8;
+        int value = (int) num;
+
+        System.out.println(value);
+    }
+}
+
