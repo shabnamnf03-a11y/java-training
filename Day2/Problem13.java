3aa5d702530181bb25872ee8bf9c30023d29e484
Shabnam <shabnamnf03@gmail.com>
2026-06-30 15:31:48 +0530

    Initial

---
+++ Day2/Problem13.java
@@ -0,0 +1,18 @@
+package Day2;
+
+public class Problem13 {
+    
+    public static void main(String[] args) {
+        String name = "Shabnam";
+        int age = 19;
+        char grade = 'A';
+        float cgpa = 8.7f;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Grade: " + grade);
+        System.out.println("CGPA: " + cgpa);
+    }
+}
+
+
