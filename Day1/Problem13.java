bf3d8a651a4d34966321356620af5f79c63ebe68
Shabnam <shabnamnf03@gmail.com>
2026-06-30 14:36:36 +0530

    Initial

---
+++ Day1/Problem13.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class Problem13 {
+    
+    public static void main(String[] args) {
+        String s = new String("Java");
+        System.out.println(s);
+    }
+}
+    
+
