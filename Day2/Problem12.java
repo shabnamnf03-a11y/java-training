e96a759014aad0ecdb6d1125cab063792f2fc007
Shabnam <shabnamnf03@gmail.com>
2026-06-30 15:29:25 +0530

    Initial

---
+++ Day2/Problem12.java
@@ -0,0 +1,13 @@
+package Day2;
+
+public class Problem12 {
+  
+    public static void main(String[] args) {
+        System.out.println(Byte.SIZE);
+        System.out.println(Short.SIZE);
+        System.out.println(Integer.SIZE);
+        System.out.println(Long.SIZE);
+    }
+}
+    
+
