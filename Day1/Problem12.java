ed0b04175228185d2e6fbadfd1e1a8c2595204d6
Shabnam <shabnamnf03@gmail.com>
2026-06-30 14:34:28 +0530

    Initial

---
+++ Day1/Problem12.java
@@ -0,0 +1,12 @@
+package Day1;
+
+public class Problem12 {
+    static {
+        System.out.println("Static Block");
+    }
+    public static void main(String[] args) {
+        System.out.println("Main Method");
+    }
+}
+    
+
