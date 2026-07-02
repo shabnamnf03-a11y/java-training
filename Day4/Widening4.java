f636b278ee89a3913136c7fd07ab7fbddba4b912
Shabnam <shabnamnf03@gmail.com>
2026-07-02 15:28:27 +0530

    Initial

---
+++ Day4/Widening4.java
@@ -0,0 +1,11 @@
+package Day4;
+
+public class Widening4 {
+    public static void main(String[] args) {
+        float f = 12.5f;
+        double d = f;
+
+        System.out.println(d);
+    }
+}
+
