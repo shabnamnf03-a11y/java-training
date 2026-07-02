e5e864b14efb2f0fd63ba4a2bfa0baf15f121ca3
Shabnam <shabnamnf03@gmail.com>
2026-07-02 14:00:18 +0530

    Initial

---
+++ Day3/Problem2.java
@@ -0,0 +1,13 @@
+package Day3;
+
+public class Problem2 {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+    }
+}
+
+
