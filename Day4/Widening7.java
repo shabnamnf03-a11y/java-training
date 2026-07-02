661b03c7315b1baf6267524ed97c64aede73a637
Shabnam <shabnamnf03@gmail.com>
2026-07-02 15:44:29 +0530

    Initial

---
+++ Day4/Widening7.java
@@ -0,0 +1,11 @@
+package Day4;
+
+public class Widening7 {
+    public static void main(String[] args) {
+        long l = 1000;
+        int i = (int) l;
+
+        System.out.println(i);
+    }
+}
+
