f7d840f6147bd0ea4a6e83fbfe098b4f7f3f4b15
Shabnam <shabnamnf03@gmail.com>
2026-07-02 11:15:18 +0530

    Initial

---
+++ Day3/Problem1.java
@@ -0,0 +1,14 @@
+package Day3;
+
+public class Problem1 {
+    
+    public static void main(String[] args) {
+        String s = "s";
+
+        if (s.equals("s") || s.equals("z")) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
