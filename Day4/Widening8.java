aac03bcd5858180bfa3c36111cecceb3f655979a
Shabnam <shabnamnf03@gmail.com>
2026-07-02 15:46:01 +0530

    Initial

---
+++ Day4/Widening8.java
@@ -0,0 +1,10 @@
+package Day4;
+
+public class Widening8 {
+    public static void main(String[] args) {
+        int num = 120;
+        byte b = (byte) num;
+
+        System.out.println(b);
+    }
+}
