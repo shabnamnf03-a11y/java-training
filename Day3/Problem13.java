dac4a531094b6274927c3058a8b1ae7726c2c5ed
Shabnam <shabnamnf03@gmail.com>
2026-07-02 14:27:33 +0530

    Initial

---
+++ Day3/Problem13.java
@@ -0,0 +1,13 @@
+package Day3;
+
+public class Problem13 {
+    public static void main(String[] args) {
+        int n = 8;
+        if (n % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+    }
+}
+    
+
